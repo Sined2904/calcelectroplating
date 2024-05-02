8971b97385c3b2fa0aa3a12b752e346091b1367a
Денис Пиневич <Den2904@yandex.ru>
2024-05-02 19:20:33 +0300

    udp

--- calc/api/urls.py
+++ calc/api/urls.py
@@ -9,7 +9,7 @@ app_name = 'api'
 router = DefaultRouter()
 
 
-router.register('time', TimeViewSet, basename='time')
+router.register('qwertime', TimeViewSet, basename='time')
 router.register('el_eqts', ElectrochemicalEquivalentsViewSet, basename='el_eqts')
 router.register('height', HeightViewSet, basename='height')
 router.register('mass', WeightViewSet, basename='mass')
